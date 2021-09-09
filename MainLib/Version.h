6c7a21ccd89b533e48e84fab5e972accac14c757
scottaronbloom <scott@towel42.com>
2021-09-09 09:49:33 -0700

    fix name

---
+++ MainLib/Version.h
@@ -0,0 +1,17 @@
+#ifndef __VERSION_H
+#define __VERSION_H
+
+#include <QString>
+
+namespace NVSProjectMaker
+{
+    inline const int MAJOR_VERSION = 1;
+    inline const int MINOR_VERSION = 29;
+    inline const int PATCH_VERSION = 0;
+
+    inline QString getVersionString() { return QString( "%1.%2" ).arg( MAJOR_VERSION ).arg( MINOR_VERSION ) ; }
+    inline QString getAppName() { return "VSProjectMaker"; }
+}
+
+#endif
+
